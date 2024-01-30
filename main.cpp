de0923a8bb1818d3fae44cd458940a2bb2d9700c
Bilal <42200647+Bm3Smartboy@users.noreply.github.com>
2024-01-30 20:48:04 +0300

    matrix that add criss cross,vertical sum,horizontal sum and other combinations

--- main.cpp
+++ main.cpp
@@ -1,28 +1,54 @@
-#include "iostream"
-
+#include<iostream>
 using namespace std;
-int main(){
-    int array_size;//size of the array
-    cout<<"enter the sizeof array:\t";
-    cin>>array_size;
-    int array[array_size];//array
-    int sum = 0;//sum of the number of the arrays
-    //receiving number from the users
-    for(int i = 0; i < array_size; i++){
-        cout<<"enter the " << i + 1 << " number:";
-        cin >> array[i];
-    }
-    //adding the number until 0 is reached and
-    //showing the result
 
-    for(int i = 0,j = 1; i < array_size; i++){//adding j for number of summation in every zero the incounter
-        sum += array[i];//adding the element
-        if (array[i] == 0) { //checking if array list reach at zero
-            cout <<"the "<< j << " sum = " << sum;//showing the result
-            sum = 0;//returning zero to its original form
+int main()
+{   int size;
+    cout << "enter the size of square matrice  \n the size must be odd number:  " ;
+    cin >> size;
+
+    int matrix[size][size];
+    cout << "enter elements of matrix" << endl;
+    for(int outer=0; outer<size; outer++) {
+        for(int inner=0; inner<size; inner++) {
+            cin >>matrix[outer][inner];
         }
     }
-    cout << "\nthe last sum = " << sum;//showing the resul at the end
+    cout << " here is a"<< size<<" by"<< size<<" matrice" << endl;
+    for(int outer=0; outer<size; outer++) {
+        for(int inner=0; inner<size; inner++) {
+            cout<<matrix[outer][inner]<<"  ";
+        }
+        cout<<endl;
+    }
 
+    int sum1=0,sum2=0,sum3=0,sum4=0;
 
+    for(int i=0; i<size; i++) {
+        int d1=(size/2);
+        sum1+=matrix[i][d1];/* vertical sum of  elements
+                         crossing the center
+              */
+
+        for(int j=0; j<size; j++) {
+            if(i==j)
+                sum2+=matrix[i][j];/*sum of elements on the left-top to
+              right-bottom diagonal
+              */
+            if((i+j)==size-1)
+                sum3+=matrix[i][j];/*sum of elements on the left-top to
+              right-bottom diagonal
+              */
+        }
+        int d2=(size/2);
+        sum4+=matrix[d2][i];/* horizontal  sum of  elements
+                         crossing the center
+              */
+
+    }
+    cout<<"vertical sum of  elements crossing the center = "<<sum1<<endl;
+    cout<<"sum of elements on the left-top toright-bottom diagonal="<<sum2<<endl;
+    cout<<"sum of elements on the right-top to left-bottom diagonal="<<sum3<<endl;
+    cout<<"horizontal  sum of  elements  crossing the center="<<sum4<<endl;
+
+    return 0;
 }
