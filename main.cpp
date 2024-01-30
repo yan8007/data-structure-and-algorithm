0c26e24ec1f910f4ab9dfa48def4a02c1de5f4af
Bilal <42200647+Bm3Smartboy@users.noreply.github.com>
2024-01-30 21:00:37 +0300

    linear search

--- main.cpp
+++ main.cpp
@@ -1,54 +1,21 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-
-int main()
-{   int size;
-    cout << "enter the size of square matrice  \n the size must be odd number:  " ;
-    cin >> size;
-
-    int matrix[size][size];
-    cout << "enter elements of matrix" << endl;
-    for(int outer=0; outer<size; outer++) {
-        for(int inner=0; inner<size; inner++) {
-            cin >>matrix[outer][inner];
-        }
+int main(){
+    int length,key;
+    cout <<"Enter the length of th array: ";
+    cin >> length;
+    int A[length];
+    for(int x: A){
+        cin>>x;
     }
-    cout << " here is a"<< size<<" by"<< size<<" matrice" << endl;
-    for(int outer=0; outer<size; outer++) {
-        for(int inner=0; inner<size; inner++) {
-            cout<<matrix[outer][inner]<<"  ";
-        }
-        cout<<endl;
+    cout << "Enter the key: ";
+    cin >> key;
+    for( int i = 0; i < length ; i++ )
+    {
+        if( key == A[ i ] ) {
+            cout << "key found at index" << i;
+            return 0;
+        }//if search is successful it ends here
     }
-
-    int sum1=0,sum2=0,sum3=0,sum4=0;
-
-    for(int i=0; i<size; i++) {
-        int d1=(size/2);
-        sum1+=matrix[i][d1];/* vertical sum of  elements
-                         crossing the center
-              */
-
-        for(int j=0; j<size; j++) {
-            if(i==j)
-                sum2+=matrix[i][j];/*sum of elements on the left-top to
-              right-bottom diagonal
-              */
-            if((i+j)==size-1)
-                sum3+=matrix[i][j];/*sum of elements on the left-top to
-              right-bottom diagonal
-              */
-        }
-        int d2=(size/2);
-        sum4+=matrix[d2][i];/* horizontal  sum of  elements
-                         crossing the center
-              */
-
-    }
-    cout<<"vertical sum of  elements crossing the center = "<<sum1<<endl;
-    cout<<"sum of elements on the left-top toright-bottom diagonal="<<sum2<<endl;
-    cout<<"sum of elements on the right-top to left-bottom diagonal="<<sum3<<endl;
-    cout<<"horizontal  sum of  elements  crossing the center="<<sum4<<endl;
-
-    return 0;
+    cout << "key not found";// if search unsuccessful returns -1
 }
