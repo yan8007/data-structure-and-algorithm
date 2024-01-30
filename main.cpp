7c3556a7a044e1f4f15e0a794262be35b73536d7
Bilal <42200647+Bm3Smartboy@users.noreply.github.com>
2024-01-30 21:08:55 +0300

    buble sorting with struct array

--- main.cpp
+++ main.cpp
@@ -1,60 +1,40 @@
-#include<stdio.h>
-struct Array
-{
-    int A[10];
-    int size;
-    int length;
-};
-void Display(struct Array arr)
-{
-    int i;
-    printf("\nElements are\n");
-    for(i=0;i<arr.length;i++)
-        printf("%d ",arr.A[i]);
-}
+#include <stdio.h>
+#include<stdlib.h>
 void swap(int *x,int *y)
 {
     int temp=*x;
     *x=*y;
     *y=temp;
 }
-int BinarySearch(struct Array arr,int key)
+void Bubble(int A[],int n)
 {
-    int l,mid,h;
-    l=0;
-    h=arr.length-1;
-    while(l<=h)
-    {
-        mid=(l+h)/2;
-        if(key==arr.A[mid])
-            return mid;
-        else if(key<arr.A[mid])
-            h=mid-1;
-        else
-            l=mid+1;
-    }
-    return -1;
-}
-int RBinSearch(int a[],int l,int h,int key)
-{
+    int i,j,flag=0;
 
-    int mid=0;
-    if(l<=h)
+    for(i=0;i<n-1;i++)
     {
-        mid=(l+h)/2;
-        if(key==a[mid])
-            return mid;
-        else if(key<a[mid])
-            return RBinSearch(a,l,mid-1,key);
+        flag=0;
+        for(j=0;j<n-i-1;j++)
+        {
+            if(A[j]>A[j+1])
+            {
+                swap(&A[j],&A[j+1]);
+                flag=1;
+            }
+        }
+        if(flag==0)
+            break;
     }
-    else
-        return RBinSearch(a,mid+1,h,key);
-    return -1;
+
 }
 int main()
 {
-    struct Array arr1={{2,3,9,16,18,21,28,32,35},10,9};
-    printf("%d",BinarySearch(arr1,16));
-    Display(arr1);
+    int A[]={11,13,7,12,16,9,24,5,10,3},n=10,i;
+
+    Bubble(A,n);
+
+    for(i=0;i<10;i++)
+        printf("%d ",A[i]);
+    printf("\n");
+
     return 0;
 }
